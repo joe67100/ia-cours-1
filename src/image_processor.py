a86d3f9a58f7456eaf2e3ba62babd27c25c2a50c
Joé FEUCHT <71235356+joe67100@users.noreply.github.com>
2024-11-01 12:10:10 +0100

    testing pre commit

--- src/image_processor.py
+++ src/image_processor.py
@@ -1,7 +1,7 @@
 class ImageProcessor:
 
-    def __init__(self, path_to_images_folder):
+    def __init__(self, path_to_images_folder: str) -> None:
         self.path_to_images_folder = path_to_images_folder
 
-    def Process_Folder(self):
+    def process_folder(self):
         pass
