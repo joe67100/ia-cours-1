0a0ea0e34351a7fcae61791eb516695f9f9dc143
Joé FEUCHT <71235356+joe67100@users.noreply.github.com>
2024-11-01 11:59:55 +0100

    Remove .idea folder from repository

---
+++ src/image_processor.py
@@ -0,0 +1,6 @@
+class ImageProcessor:
+    def __init__(self, path_to_images_folder: str) -> None:
+        self.path_to_images_folder = path_to_images_folder
+
+    def process_folder(self):
+        pass
