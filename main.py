edb127953df636a3f2fc2fe16fa20c7e40a6fd1d
Joé FEUCHT <71235356+joe67100@users.noreply.github.com>
2024-11-02 12:34:04 +0100

    chore: removed comment

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import os
 def main():
     image_processor = ImageProcessor(f"{os.getcwd()}/input_images")
     image_processor.process_folder()
-    # image_processor.process_image("input_images/000000000009.jpg", "output_images")
 
 
 if __name__ == "__main__":
